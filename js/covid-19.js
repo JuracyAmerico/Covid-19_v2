c78b25200d4a2a8aeb0007150dc34913ccb35b8a
Jura <juracyamerico@gmail.com>
2020-05-28 07:52:42 -0300

    Update covid-19.js

--- js/covid-19.js
+++ js/covid-19.js
@@ -16,12 +16,12 @@
     // Definição do esquema
     myConnector.getSchema = function(schemaCallback) {
         var casos_colunas = [            
-        {id:"city", alias: "Cidade", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.county , description: "nome do município (pode estar em branco quando o registro é referente ao estado, pode ser preenchido com Importados/Indefinidos também"},
+        {id:"city", alias: "Cidade", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.county},
         {id:"city_ibge_code", alias: "Cd Mun", dataType: tableau.dataTypeEnum.string, columnRole: tableau.columnRoleEnum.dimension},
         {id:"confirmed", alias: "Confirmados", dataType: tableau.dataTypeEnum.float},
         {id:"confirmed_per_100k_inhabitants", alias: "Confirmados por 100k hab.", dataType: tableau.dataTypeEnum.int},
         {id:"date", alias: "Data", dataType: tableau.dataTypeEnum.date},
-        {id:"death_rate", alias: "Mortes/confirmados", dataType: tableau.dataTypeEnum.float},
+        {id:"death_rate", alias: "Mortes/confirmados", dataType: tableau.dataTypeEnum.float, aggType: tableau.aggTypeEnum.avg, numberFormat: tableau.numberFormatEnum.percentage},
         {id:"deaths", alias: "Fatalidades", dataType: tableau.dataTypeEnum.float},
         {id:"estimated_population_2019", alias: "População estimada 2019", dataType: tableau.dataTypeEnum.int},
         {id:"is_last", alias: "É a última atualização?", dataType: tableau.dataTypeEnum.string},
@@ -32,9 +32,34 @@
         var boletim_colunas = [
         {id:"date", alias: "Data", dataType: tableau.dataTypeEnum.date},
         {id:"notes", alias: "Notes", dataType: tableau.dataTypeEnum.string},
-        {id:"state", alias: "Estado", dataType: tableau.dataTypeEnum.string},
+        {id:"state", alias: "Estado", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.state_province},
         {id:"url", alias: "Url", dataType: tableau.dataTypeEnum.string}
         ];
+
+        var caso_full_colunas = [
+        {id:"source_id", alias:"source_id", dataType: tableau.dataTypeEnum.string},
+        {id:"dataSintomas", alias:"dataSintomas", dataType: tableau.dataTypeEnum.datetime},
+        {id:"dataRegistro", alias:"dataRegistro", dataType: tableau.dataTypeEnum.datetime},
+        {id:"dataAtualizacao", alias:"dataAtualizacao", dataType: tableau.dataTypeEnum.datetime},
+        {id:"dataNascimento", alias:"dataNascimento", dataType: tableau.dataTypeEnum.datetime},
+        {id:"numeroNotificacao", alias:"numeroNotificacao", dataType: tableau.dataTypeEnum.string},
+        {id:"municipio", alias:"municipio", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.county},
+        {id:"estado", alias:"estado", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.state_province},
+        {id:"municipioNotificacao", alias:"municipioNotificacao", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.county},
+        {id:"estadoNotificacao", alias:"estadoNotificacao", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.state_province},
+        {id:"cep", alias:"cep", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.zip_code_postcode},
+        {id:"comorbidades", alias:"comorbidades", dataType: tableau.dataTypeEnum.bool},
+        {id:"numeroCnes", alias:"numeroCnes", dataType: tableau.dataTypeEnum.string},
+        {id:"confirmado", alias:"confirmado", dataType: tableau.dataTypeEnum.bool},
+        {id:"sexo", alias:"sexo", dataType: tableau.dataTypeEnum.string},
+        {id:"excluido", alias:"excluido", dataType: tableau.dataTypeEnum.bool},
+        {id:"fonte", alias:"fonte", dataType: tableau.dataTypeEnum.string},
+        {id:"idade", alias:"idade", dataType: tableau.dataTypeEnum.int},
+        {id:"febre", alias:"febre", dataType: tableau.dataTypeEnum.bool},
+        {id:"sinaisGravidade", alias:"sinaisGravidade", dataType: tableau.dataTypeEnum.bool},
+        {id:"sinaisRespiratorios", alias:"sinaisRespiratorios", dataType: tableau.dataTypeEnum.bool}
+        //{id:"@timestamp", alias:"@timestamp", dataType: tableau.dataTypeEnum.datetime}            
+        ]
 
         var obito_cartorio_colunas = [
         {id:"date", alias:"date", dataType: tableau.dataTypeEnum.date},
@@ -50,33 +75,40 @@
         {id:"new_deaths_pneumonia_2020", alias:"new_deaths_pneumonia_2020", dataType: tableau.dataTypeEnum.int},
         {id:"new_deaths_respiratory_failure_2019", alias:"new_deaths_respiratory_failure_2019", dataType: tableau.dataTypeEnum.int},
         {id:"new_deaths_respiratory_failure_2020", alias:"new_deaths_respiratory_failure_2020", dataType: tableau.dataTypeEnum.int},
-        {id:"state", alias:"state", dataType: tableau.dataTypeEnum.string}
+        {id:"state", alias:"state", dataType: tableau.dataTypeEnum.string, geoRole: tableau.geographicRoleEnum.state_province}
         ]
 
         //Definicao da Tabela
-        var casosTabela = {
+        var casos_tabela = {
             id: 'casos',
             alias: 'casos',
             description: 'Essa tabela tem apenas os casos relatados pelos boletins das Secretarias Estaduais de Saúde e, por isso, não possui valores para todos os municípios e todas as datas - é nossa "tabela canônica", que reflete o que foi publicado.',
             columns: casos_colunas
         };
 
-        var boletim_colunasTabela = {
+        var boletim_tabela = {
             id: 'boletim',
             alias: 'boletim',
-            description: 'Tabela que lista os boletins publicados pelas Secretarias Estaduais de Saúde. Pode aparecer mais de um para a mesma data e podem existir dias em que as SES não publicam boletins',
+            description: 'Tabela que lista os boletins publicados pelas Secretarias Estaduais de Saúde. Pode aparecer mais de um para a mesma data e podem existir dias em que as SES não publicam boletins.',
             columns: boletim_colunas
         };
 
-        var obito_cartorio_colunasTabela = {
+        var caso_full_tabela = {
+            id: 'caso_full',
+            alias: 'caso_full',
+            description: 'Tabela gerada a partir da tabela caso, que possui um registro por município (+ Importados/Indefinidos) e estado para cada data disponível; nos casos em que um boletim não foi divulgado naquele dia, é copiado o dado do último dia disponível e a coluna is_repeated fica com o valor True.',
+            columns: caso_full_colunas
+        };
+        
+        var obito_cartorio_tabela = {
             id: 'obito_cartorio',
             alias: 'obito_cartorio',
-            description: 'Essa tabela contém dados de óbitos registrados nos cartórios e disponíveis no Portal da Transparência do Registro Civil',
+            description: 'Essa tabela contém dados de óbitos registrados nos cartórios e disponíveis no Portal da Transparência do Registro Civil.',
             columns: obito_cartorio_colunas
         };        
 
 
-        schemaCallback([casosTabela, boletim_colunasTabela, obito_cartorio_colunasTabela]);
+        schemaCallback([casos_tabela, boletim_tabela, caso_full_tabela, obito_cartorio_tabela]);
     };
 
     //3: *************************
@@ -158,6 +190,59 @@
             getAllData(url);  
         }
         
+        //Acessando dados dos caso_full
+        if (table.tableInfo.id == 'caso_full') {        
+            function getAllData(url) {  
+            $.getJSON(url, function(resp) {  
+                var feat = resp.results,  
+                next = resp.next;  
+        
+                // Interagindo no objeto JSON  
+        
+                for (var i = 1, len = feat.length; i < len; i++) {  
+                tableData.push({  
+                    "source_id": feat[i].source_id,
+                    "dataSintomas": feat[i].dataSintomas,
+                    "dataRegistro": feat[i].dataRegistro,
+                    "dataAtualizacao": feat[i].dataAtualizacao,
+                    "dataNascimento": feat[i].dataNascimento,
+                    "numeroNotificacao": feat[i].numeroNotificacao,
+                    "municipio": feat[i].municipio,
+                    "estado": feat[i].estado,
+                    "municipioNotificacao": feat[i].municipioNotificacao,
+                    "estadoNotificacao": feat[i].estadoNotificacao,
+                    "cep": feat[i].cep,
+                    "comorbidades": feat[i].comorbidades,
+                    "numeroCnes": feat[i].numeroCnes,
+                    "confirmado": feat[i].confirmado,
+                    "sexo": feat[i].sexo,
+                    "excluido": feat[i].excluido,
+                    "fonte": feat[i].fonte,
+                    "idade": feat[i].idade,
+                    "febre": feat[i].febre,
+                    "sinaisGravidade": feat[i].sinaisGravidade,
+                    "sinaisRespiratorios": feat[i].sinaisRespiratorios
+                    //"@timestamp": feat[i].@timestamp
+                                         
+                });  
+                }  
+                // Interagindo entre todas as paginas, para isso fazemos a variavel next = resp.next; para pegar proxima pagina com dados , se nao encontrar mais dados vai ser null
+                // https://community.tableau.com/thread/335501 contribuição da Keshia Rose
+                if (next !== null) {  
+                getAllData(next);  
+                } else {  
+                table.appendRows(tableData);  
+                doneCallback();  
+                }  
+            });  
+            }
+        
+      
+            var tableData = [];  
+            var url = "https://brasil.io/api/dataset/covid19/caso_full/data?page=1";  
+            getAllData(url);  
+        }          
+
         //Acessando dados dos obito_cartorio
         if (table.tableInfo.id == 'obito_cartorio') {        
             function getAllData(url) {  
@@ -201,7 +286,7 @@
             var tableData = [];  
             var url = "https://brasil.io/api/dataset/covid19/obito_cartorio/data?page=1";  
             getAllData(url);  
-        }          
+        }        
     }; 
 
     //4: *************************
